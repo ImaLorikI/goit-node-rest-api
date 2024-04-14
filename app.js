128d025a3f711e4aaff5fc7cd9d5feef3600f2ec
ImaLorikI <pasha.bandrivskiy@gmail.com>
2024-04-15 01:43:21 +0300

    1

--- app.js
+++ app.js
@@ -2,9 +2,23 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 import contactsRouter from "./routes/contactsRouter.js";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
+dotenv.config();
 
 const app = express();
+
+
+mongoose
+  .connect(process.env.MongoDB_URL)
+  .then(() => {
+    console.log("mongoDb connected");
+  })
+  .catch((err) => {
+    console.log(err);
+    process.exit(1);
+  });
 
 app.use(morgan("tiny"));
 app.use(cors());
